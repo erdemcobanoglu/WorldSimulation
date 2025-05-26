ed4bf9beac5d1116ca8aa6a28acc5d95ae21cc53
erdem çobanoglu <erdemcobanoglu1122@hotmail.com>
2025-05-26 15:41:39 +0100

    add WorldEvent

---
+++ WorldSimulation.Application/Interfaces/IWorldEvent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldSimulation.Application.Interfaces
+{
+    public interface IWorldEvent
+    {
+        string EventType { get; }
+        int X { get; }
+        int Y { get; }
+        double Intensity { get; }
+        int Duration { get; }
+        DateTime StartTime { get; }
+    }
+}
